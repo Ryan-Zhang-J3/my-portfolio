a3a7f38049c3ae23240ad0626dae09cf70c11e55
Ryan <ryan.zhang.formal@gmail.com>
2024-01-08 16:46:10 -0500

    final commit

--- src/lib/projects.params.ts
+++ src/lib/projects.params.ts
@@ -10,7 +10,7 @@ const MY_PROJECTS: Array<Project> = [
 			'Giftver is an easy to use web application that allows friends to create, manage, and run Secret Santa Gift Exchanges from anywhere.',
 		shortDescription:
 			'Giftver is an easy to use web application that allows friends to create, manage, and run Secret Santa Gift Exchanges from anywhere.',
-		links: [{ to: 'https://github.com/Ryan-Zhang-J3/Giftver', label: 'GitHub' }],
+		links: [{ to: 'https://github.com/Ryan-Zhang-J3/Giftver', label: 'GitHub' }, { to: 'https://youtu.be/EPajGnAjCQI', label: 'Video Demo'}],
 		logo: Assets.Giftver,
 		name: 'Giftver',
 		period: {
@@ -25,7 +25,7 @@ const MY_PROJECTS: Array<Project> = [
 			},
 			{
 				label: 'Exchange Management',
-				src: 'https://private-user-images.githubusercontent.com/122586539/293572250-3209087c-4acd-4f41-a771-de34f8370dcf.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ1MTQxMTksIm5iZiI6MTcwNDUxMzgxOSwicGF0aCI6Ii8xMjI1ODY1MzkvMjkzNTcyMjUwLTMyMDkwODdjLTRhY2QtNGY0MS1hNzcxLWRlMzRmODM3MGRjZi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTA2JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEwNlQwNDAzMzlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT03YzcwNzA1YmNlZGZiNDllZDIzNzY0MjY2YjkyZmZhNzBjNTQ2ZDdlMTg3NDg0YmJiNWU4YmZlZTVlNDhjNTExJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.6mIw_xkU07BuIFvMglBSAFd_7IFCQZ4AP6KdxgQGKz4'
+				src: 'https://private-user-images.githubusercontent.com/122586539/293572250-3209087c-4acd-4f41-a771-de34f8370dcf.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ3NDk5ODIsIm5iZiI6MTcwNDc0OTY4MiwicGF0aCI6Ii8xMjI1ODY1MzkvMjkzNTcyMjUwLTMyMDkwODdjLTRhY2QtNGY0MS1hNzcxLWRlMzRmODM3MGRjZi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTA4JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEwOFQyMTM0NDJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT03M2JjZWZlMmI1Yjk2MDRmY2M5OGZmMGJkNzJkNTQ5Mzk1NTYyMmIzNWU1ZTZhZDk3NjkxZjNmYjkxNmJlMTIwJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.-UQh6UTY9zyT5GXN70i5GXKl975SVM8hKKRAGqzVTrM'
 			},
 			{
 				label: 'Recieving your secret santa recipient',
